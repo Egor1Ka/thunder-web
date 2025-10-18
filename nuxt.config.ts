0ad0c1bde0703144d4a760785dda130df1ae4c3a
Zozulia <Zozulia@908.dp.ua>
2025-10-18 19:59:16 +0300

    added google analityc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  modules: ["@nuxt/image", "usebootstrap", "nuxt-icons"],
+  modules: ["@nuxt/image", "usebootstrap", "nuxt-icons", "nuxt-gtag"],
   css: ["bootstrap/dist/css/bootstrap.min.css", "~/assets/css/main.css"],
   image: {
     domains: ["drive.google.com", "lh3.googleusercontent.com"],
   },
-  buildModules: ["@nuxtjs/google-analytics"],
-  googleAnalytics: {
+  gtag: {
     id: "G-C9QRWM34NG",
-  },
-  publicRuntimeConfig: {
-    googleAnalytics: {
-      id: process.env.GOOGLE_ANALYTICS_ID,
-    },
   },
 });
